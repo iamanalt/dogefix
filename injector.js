cc90929a28f878bd3a4e374764758a6eecde1dd2
Prokop <prokop.schield@protonmail.com>
2021-05-17 16:04:54 +0200

    Add boilerplate

--- injector.js
+++
@@ -1,6 +0,0 @@
-let script = document.createElement('script');
-script.src = chrome.extension.getURL('injected.js');
-script.onload = function() {
-    this.parentNode.removeChild(this);
-};
-(document.head || document.documentElement).appendChild(script);
